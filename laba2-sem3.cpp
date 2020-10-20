12796cb7337c0eb33c32e76cdb3d7d70c835d829
itimur317 <56135499+itimur317@users.noreply.github.com>
2020-10-21 02:34:47 +0300

    Add files via upload

--- laba2-sem3.cpp
+++ laba2-sem3.cpp
@@ -6,13 +6,8 @@
 #include "test_dictionary.h"
 #include "test_sparce_vector.h"
 #include "test_histogram.h"
-#include "Search_often_sub.h"
+#include "test_search_often_sub.h"
 
-int compare_string_int(Tpair_for_dict<string, int> a, Tpair_for_dict<string, int> b) {
-    if (a.key > b.key)
-        return 1;
-    return 0;
-}
 
 
 int main()
@@ -23,36 +18,8 @@ int main()
     TEST_ALL_DICTIONARY();
     TEST_ALL_SPARCE_VECTOR();
     TEST_ALL_HISTOGRAM();
+    TEST_ALL_SEARCH_OFTEN_SUB();
     
-    ArraySequence<char> arr;
-    arr.Append('a');
-    arr.Append('b');
-    arr.Append('c');
-    arr.Append('a');
-    arr.Append('a');
-    arr.Append('a');
-    arr.Append('b');
-    arr.Append('a');
-    arr.Append('b');
-    arr.Append('c');
-    arr.Append('a');
-    arr.Append('c');
-    arr.Append('a');
 
-    Sequence<char>* seq;
-    seq = &arr;
-
-    Search_often_sub blyat(seq, 2, 3, compare_string_int);//, bubble_sort, compare_sorted_decrease);
-    cout << blyat.Get("abc") << endl;
-    cout << blyat.Get("ca") << endl;
-    cout << blyat.Get("bab") << endl;
-    cout << blyat.Get("ab") << endl;
-    cout << blyat.Get_max_count() << "  "  << endl;
-    blyat.print();
-    cout << "dadadadawddawd" << endl;
-    Sequence<Tpair_for_dict<string, int>>* new_seq = blyat.output_seq();
-    print_seq(new_seq);
-    cout << blyat.Get_max_count() << endl;
-    print_seq(blyat.Get_max());
     return 0;
 }
